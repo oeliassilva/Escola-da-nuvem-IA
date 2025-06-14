fe36ec7c8f7ddaa729b0bca2621a06c997570f0e
Elias Santos <elsantos@id.uff.br>
2025-06-13 23:16:30 -0300

    Alteração do print do exercício calculadora_de_preco_total

--- Aula 1/ex4_calculadora_de_preco_total.py
+++ Aula 1/ex4_calculadora_de_preco_total.py
@@ -3,4 +3,4 @@ preco_unitario = 12.40
 quantidade = 3
 
 preco_total = preco_unitario * quantidade
-print(f"O preço total do produto {produto} é: R$ {preco_total:.2f}")
+print(f"O preço total das {quantidade} unidades da {produto} é de: R$ {preco_total:.2f}")
