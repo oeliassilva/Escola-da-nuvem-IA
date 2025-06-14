3da53edb591670cef5af15fcdf673b1d9fd98555
Elias Santos <elsantos@id.uff.br>
2025-06-13 22:58:42 -0300

    Commit dos exercicios da aula 1

--- Aula 1/Multiplicacao_e_exponeciacao.py
+++
@@ -1,7 +0,0 @@
-comprimento = int(input("Digite o comprimento do objeto em cm: "))
-largura = int(input("Digite o largura do objeto em cm: "))
-altura = int(input("Digite a altura do objeto em cm: "))
-
-volume = comprimento * largura * altura
-print(f"O volume do objeto é: {volume} cm³")
-
---
+++ Aula 1/ex4_calculadora_de_preco_total.py
@@ -0,0 +1,6 @@
+produto = "Caideira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+print(f"O preço total do produto {produto} é: R$ {preco_total:.2f}")
